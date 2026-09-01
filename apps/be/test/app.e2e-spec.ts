e7d22f8073aa6b7e82558e5eb0e188f7e07d8f2b
daniel-majer <dano.majer@protonmail.com>
2026-09-01 22:12:03 +0200

    feat(be): implement fastify engine

--- apps/be/test/app.e2e-spec.ts
+++ apps/be/test/app.e2e-spec.ts
@@ -1,26 +1,31 @@
-import { INestApplication } from "@nestjs/common";
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from "@nestjs/platform-fastify";
 import { Test, TestingModule } from "@nestjs/testing";
-import request from "supertest";
 
 import { AppModule } from "./../src/app.module.js";
 
 describe("AppController (e2e)", () => {
-  let app: INestApplication;
+  let app: NestFastifyApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
+    app = moduleFixture.createNestApplication<NestFastifyApplication>(
+      new FastifyAdapter(),
+    );
     await app.init();
+    await app.getHttpAdapter().getInstance().ready();
   });
 
-  it("/ (GET)", () => {
-    return request(app.getHttpServer())
-      .get("/")
-      .expect(200)
-      .expect("Hello World!");
+  it("/ (GET)", async () => {
+    const res = await app.inject({ method: "GET", url: "/" });
+
+    expect(res.statusCode).toBe(200);
+    expect(res.payload).toBe("Hello World!");
   });
 
   afterEach(async () => {
